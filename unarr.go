0f0ff4d526157a03f871e8ed407eaebc0ad4f12d
Rafal <radolasd@gmail.com>
2019-03-14 17:42:49 +0000

    remove ByteCountDecimal

--- unarr.go
+++ unarr.go
@@ -31,19 +31,6 @@ type Item struct {
 	Size int64
 }
 
-func ByteCountDecimal(b int64) string {
-	const unit = 1000
-	if b < unit {
-		return fmt.Sprintf("%d B", b)
-	}
-	div, exp := int64(unit), 0
-	for n := b / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
-}
-
 // NewArchive returns new unarr Archive
 func NewArchive(path string) (a *Archive, err error) {
 	a = new(Archive)
